9751aec79ed2eae16d9e6145a72e76ca11e3f058
Perets Viktoria <vktrprc@gmail.com>
2026-03-24 21:16:30 +0200

    Add IO functions without features

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def input_from_cons():
+    return
+
+def input_from_file_builtin():
+    return
+
+def input_from_file_pandas():
+    return
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+def output_in_cons():
+    return
+
+def output_in_file_builtin():
+    return
+
+def output_in_file_pandas():
+    return
